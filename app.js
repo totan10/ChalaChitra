40c7f92060d6d0a803dd25440620455ce1cd13ba
totan10 <79355253+totan10@users.noreply.github.com>
2022-08-24 05:50:16 +0530

    complete code

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const arrows= document.querySelectorAll(".arrow");
-const movielists = document.querySelectorAll(".movie-list");
+const movieLists = document.querySelectorAll(".movie-list");
 
-arrows.forEach((arrow,i)=>{
-    arrow.addEventListener("click",()=>{
-        movielists[i].style.transform=`translate(${
-            movielists[i].computedStyleMap().get("transform")[0].x.value -300
-        }px)`;
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio= Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter)+(4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+  console.log(Math.floor(window.innerWidth/270));
+
+});
+
+const ball=document.querySelector(".toggle-ball");
+const items=document.querySelectorAll(".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle.toggle");
+ball.addEventListener("click",()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active")
     })
-
+    ball.classList.toggle("active")
 })
